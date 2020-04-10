ce69293911fd74416c97f088be3c2bc9c2c5da84
airwolff <andy.wolff@gmail.com>
2020-04-09 20:07:51 -0500

    notes

--- ng-refresh/src/app/app.component.ts
+++ ng-refresh/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // points to html tag at root document
   selector: 'app-root',
+  // what to render when mounting component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- ng-refresh/src/app/app.module.ts
+++ ng-refresh/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
 
+// add below decorator to export class AppModule to provide metadata so behind the scenes Angular will transpile code to execute properly
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CustomersComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+  // root defined
   bootstrap: [AppComponent]
 })
 export class AppModule { }
